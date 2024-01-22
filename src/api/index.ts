cb8451ba452e3ad572c77691139c7e6b828e8823
joehannis <joe_hannis@hotmail.com>
2024-01-22 13:16:02 +0000

    feature: add tailwind

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,12 +1,14 @@
 import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Express & TypeScript Server');
 });
 
 app.listen(port, () => {
-  console.log(`Server is Fire at http://localhost:${port}`);
+  console.log(`Server is at http://localhost:${port}`);
 });
