0340397e6ef4b6170586d7fdda0b893172fd63ff
Joe Hannis <joe_hannis@hotmail.com>
2024-10-10 17:19:35 +0100

    fix vercel API config

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import express, { Express } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-import recipeRoute from '../routes/recipeRoute';
+import recipeRoute from './routes/recipeRoute';
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
